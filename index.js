26af61a8d67d3780928cde6c25943f3111f1c2d7
acsajjad705 <acsajjad705@gmail.com>
2025-12-06 21:35:14 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const userRoutes = require('./routes/users');
+const logger = require('./middleware/logger');
+const auth = require('./middleware/auth');
+
+app.use(express.json());
+app.use(logger);
+app.use(auth);
+
+app.use('/users', userRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
