18256d4cab58e79195b23d9ad73aec6f127d8eeb
Syed Sajjad Raza <acsajjad705@gmail.com>
2025-12-07 00:02:16 +0500

    Delete middleware directory

--- middleware/auth.js
+++
@@ -1,8 +0,0 @@
-function auth(req, res, next) {
-  // Simple demo auth: require header "x-api-key"
-  if (req.headers['x-api-key'] !== 'secret123') {
-    return res.status(403).json({ error: 'Unauthorized' });
-  }
-  next();
-}
-module.exports = auth;
